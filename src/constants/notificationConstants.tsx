74e2e16f7553cc98a3cd9666932373d80ed69bc6
andru-roshan <roshanjohn316640@gmail.com>
2024-05-03 18:56:18 +0530

    notification changes

---
+++ src/constants/notificationConstants.tsx
@@ -0,0 +1,56 @@
+export interface notificationItem {
+  id: number;
+  notificationTitle: string;
+  dateTime: string;
+  notificationSubTitle: string;
+  isRead: boolean;
+  notificationDescription: string;
+}
+
+export const notificationMenu: notificationItem[] = [
+  {
+    id: 1,
+    notificationTitle: "JBasso",
+    dateTime: "03/05/2024 12:54PM",
+    notificationSubTitle: "General Category - R894216",
+    isRead: true,
+    notificationDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    notificationTitle: "JBasso",
+    dateTime: "03/05/2024 12:54PM",
+    notificationSubTitle: "General Category - R894216",
+    isRead: true,
+    notificationDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    notificationTitle: "JBasso",
+    dateTime: "03/05/2024 12:54PM",
+    notificationSubTitle: "General Category - R894216",
+    isRead: false,
+    notificationDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    notificationTitle: "JBasso",
+    dateTime: "03/05/2024 12:54PM",
+    notificationSubTitle: "General Category - R894216",
+    isRead: false,
+    notificationDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 5,
+    notificationTitle: "JBasso",
+    dateTime: "03/05/2024 12:54PM",
+    notificationSubTitle: "General Category - R894216",
+    isRead: false,
+    notificationDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ export default {
       colors: {
         primary: "#2A7DBC",
         secondary: "#f0f2f6",
+        greyPrimary: "#8B8B8B",
+        primayAlert: "#EB5757",
       },
     },
     container: {
