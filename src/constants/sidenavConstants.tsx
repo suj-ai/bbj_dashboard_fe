b7bef7669c46d6c8cc3cc11ffbf85df82438ad40
sujai shanmugam <sujai.shanmugam@flyerssoft.com>
2024-05-06 10:45:23 +0530

    final-changes

--- src/constants/sidenavConstants.tsx
+++ src/constants/sidenavConstants.tsx
@@ -1,5 +1,7 @@
 import { FaRegStar } from "react-icons/fa";
 import { LiaBoxOpenSolid } from "react-icons/lia";
+import { LiaHandshakeSolid } from "react-icons/lia";
+import { MdOutlineInventory2 } from "react-icons/md";
 
 type MenuItem = {
   key: string;
@@ -33,6 +35,38 @@ export const SIDENAV_CONSTANTS: SidenavConstants = [
         key: "order_freight",
         label: "Order Freight",
         path: "/orders_management/order_freight",
+      },
+    ],
+  },
+  {
+    key: "customer_maintainance",
+    icon: LiaHandshakeSolid,
+    label: "Customer Maintainance",
+    path: "/customer_maintainance",
+    items: [
+      {
+        key: "customers",
+        label: "Customers",
+        path: "/customer_maintainance/customers",
+      },
+    ],
+  },
+
+  {
+    key: "inventory_maintainance",
+    icon: MdOutlineInventory2,
+    label: "Inventory Maintainance",
+    path: "/inventory_maintainance",
+    items: [
+      {
+        key: "items",
+        label: "Items",
+        path: "/inventory_maintainance/items",
+      },
+      {
+        key: "availablity",
+        label: "Availablity",
+        path: "/inventory_maintainance/availablity",
       },
     ],
   },
